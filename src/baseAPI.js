7514091529a7ab42577e3e5d6a0cc650fe3a9278
LUCIFER-ZHU <1147570162@qq.com>
2020-06-16 12:23:56 +0800

    首页开发完成

---
+++ src/baseAPI.js
@@ -0,0 +1,41 @@
+import wepy from 'wepy'
+
+/**
+ * 弹框提示一个无图标的 Toast 消息
+ * @str 要提示的消息内容
+ */
+wepy.baseToast = function (str = '获取数据失败！') {
+  wepy.showToast({
+    title: str,
+    // 弹框期间不会携带任何图标
+    icon: 'none',
+    duration: 1500
+  })
+}
+
+const baseURL = 'https://www.zhengzhicheng.cn/api/public/v1'
+/**
+ * 发起 get 请求的 API
+ * @url 请求的地址，为相对路径，必须以 / 开头
+ * @data 请求的参数对象
+ */
+wepy.get = function (url, data = {}) {
+  return wepy.request({
+    url: baseURL + url,
+    method: 'GET',
+    data
+  })
+}
+
+/**
+ * 发起 post 请求的 API
+ * @url 请求的地址，为相对路径，必须以 / 开头
+ * @data 请求的参数对象
+ */
+wepy.post = function (url, data = {}) {
+  return wepy.request({
+    url: baseURL + url,
+    method: 'POST',
+    data
+  })
+}
---
+++ src/mixins/tabs/home.js
@@ -0,0 +1,68 @@
+import wepy from 'wepy'
+
+// 注意，必须继承自 wepy.mixin
+export default class extends wepy.mixin {
+  data = {
+    // 轮播图数据
+    swiperList: [],
+    // 分类数据
+    cateItems: [],
+    // 楼层数据
+    floorData: []
+  }
+
+  onload() {
+    this.getSwiperData()
+    // 分类数据
+    this.getCateItems()
+    // 在页面加载完成后，自动获取楼层数据
+    this.getFloorData()
+  }
+
+  methods = {
+    // 点击楼层中的每一张图片，都要跳转到商品列表页面
+    goGoodsList(url) {
+      wepy.navigateTo({
+        url
+      })
+    }
+  }
+  // 获取轮播图数据
+  async getSwiperData() {
+    // 解构赋值
+    const { data: res } = await wepy.get('/home/swiperdata')
+
+    console.log(res)
+    if (res.mata.status !== 200) {
+      // console.log('获取数据失败')
+      return wepy.baseToast()
+    }
+
+    this.swiperList = res.message
+    this.$apply()
+  }
+
+  // 获取首页分类相关的数据项
+  async getCateItems() {
+    const { data: res } = await wepy.get('/home/catitems')
+
+    if (res.meta.status !== 200) {
+      return wepy.baseToast()
+    }
+
+    this.cateItems = res.message
+    this.$apply()
+  }
+
+  // 获取楼层相关数据
+  async getFloorData() {
+    const { data: res } = await wepy.get('/home/floordata')
+
+    if (res.meta.status !== 200) {
+      return wepy.baseToast()
+    }
+
+    this.floorData = res.message
+    this.$apply()
+  }
+}
